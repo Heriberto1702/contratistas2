8d8ffdc5a90849c6c68805f638834cd4231a9b58
Heriberto1702 <heriberto.garcia@sinsa.com.ni>
2025-01-07 15:10:01 -0600

    cambios middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,15 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
+  // Define la baseURL según el entorno
+  const baseURL =
+    process.env.NODE_ENV === "production"
+      ? "https://contratistas2.vercel.app"
+      : "http://localhost:3000";
+
   // Si no hay token, redirige a la página de login
   if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
+    return NextResponse.redirect(new URL("/login", baseURL));
   }
 
   // Permitir el acceso a la página
@@ -17,5 +23,5 @@ export async function middleware(req: NextRequest) {
 
 // Define qué rutas deben ser protegidas
 export const config = {
-  matcher: ["/academia/:path*" , "/datos/:path*" , "/compras/:path*" , "/" ], // Protege estas rutas
+  matcher: ["/academia/:path*", "/datos/:path*", "/compras/:path*", "/"], // Protege estas rutas
 };
