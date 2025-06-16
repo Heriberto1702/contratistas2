7a364448a43ed26e48332417fdecd0e4b3532779
Oscar Manuel López Mojica <lopezoscarmanolo@gmail.com>
2025-06-16 14:02:18 -0600

    tercera prueba

--- src/app/beneficios/page.tsx
+++ src/app/beneficios/page.tsx
@@ -1,5 +1,10 @@
+import React, { Suspense } from "react";
 import BeneficiosPageClient from "@/app/components/BeneficioContratista/BeneficioPageClient";
 
 export default function Page() {
+  return (
+    <Suspense fallback={<div>Cargando beneficios...</div>}>
   return <BeneficiosPageClient />;
+    </Suspense>
+  );
 }
