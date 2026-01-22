401b12961ed3dc41d9dedafe12c32473348db87a
Oscar Manuel López Mojica <lopezoscarmanolo@gmail.com>
2026-01-22 09:05:17 -0600

    next

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,22 +6,20 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
-  // Define la baseURL según el entorno
-  const baseURL =
-    process.env.NODE_ENV === "production"
-      ? "https://contratistas2.vercel.app"
-      : "http://localhost:3000";
-
-  // Si no hay token, redirige a la página de login
   if (!token) {
-    return NextResponse.redirect(new URL("/login", baseURL));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // Permitir el acceso a la página
   return NextResponse.next();
 }
 
-// Define qué rutas deben ser protegidas
 export const config = {
-  matcher: ["/academia/:path*", "/datos/:path*", "/compras/:path*", "/" ,"/usuario/:path*", "/beneficios/:path*"], // Protege estas rutas
+  matcher: [
+    "/academia/:path*",
+    "/datos/:path*",
+    "/compras/:path*",
+    "/usuario/:path*",
+    "/beneficios/:path*",
+    "/",
+  ],
 };
